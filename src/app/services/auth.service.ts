8674c5125f23b2d1895e0dfd46806007f491707d
Ram Krishna Kumar <ram.krishana36@gmail.com>
2020-02-17 16:11:45 +0530

    added live api url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -71,6 +71,9 @@ export class AuthService {
   //   });
   // }
 
+
+
+  //Perfect running confirm functions
   UserLogin(body){ 
     return new Promise((resolve,reject) => {
       //let body = 'email=' + this.validations_form.value.email + '&password=' + this.validations_form.value.password;
@@ -82,7 +85,7 @@ export class AuthService {
         'Accept': 'application/json'           
           });
           const requestOptions = new RequestOptions({ headers: headers });
-          this.http.post("http://wiesoftware.com/greenchili/apisecure/login/loginUsers/", body,requestOptions).subscribe(res => {
+          this.http.post("http://greenchili.ca/apisecure/login/loginUsers", body,requestOptions).subscribe(res => {
            resolve(res.json());
            this.response= res.json(); 
            console.log(this.response);
@@ -92,9 +95,6 @@ export class AuthService {
           });
     });
   }
-  
-  
-  
 
   SignUp(body) {
     return new Promise((resolve,reject) => {
@@ -104,7 +104,7 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.post("http://wiesoftware.com/greenchili/apisecure/login/registerUsers", body,requestOptions).subscribe(res => {
+          this.http.post("http://greenchili.ca/apisecure/login/registerUsers", body,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);            
@@ -122,7 +122,7 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.get(" http://wiesoftware.com/greenchili/apisecure/order/orderHistory/"+user_id,requestOptions).subscribe(res => {
+          this.http.get(" http://greenchili.ca/apisecure/order/orderHistory/"+user_id,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -138,7 +138,7 @@ export class AuthService {
             });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.get("http://wiesoftware.com/greenchili/apisecure/userDetails/"+user_id,requestOptions).subscribe(res => {
+          this.http.get("http://greenchili.ca/apisecure/userDetails/"+user_id,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -154,7 +154,7 @@ export class AuthService {
             });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.post("http://wiesoftware.com/greenchili/apisecure/userProfileEdit/"+user_id,body,requestOptions).subscribe(res => {
+          this.http.post("http://greenchili.ca/apisecure/userProfileEdit/"+user_id,body,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -170,7 +170,7 @@ export class AuthService {
         'Accept': 'application/json'
       });
       const requestOptions = new RequestOptions({headers: headers});
-      this.http.post("http://wiesoftware.com/greenchili/apisecure/onlineReservation",body, requestOptions).subscribe(res =>{
+      this.http.post("http://greenchili.ca/apisecure/onlineReservation",body, requestOptions).subscribe(res =>{
         resolve(res.json());
       }, (err) => {
         reject(err);
@@ -187,7 +187,7 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.post("http://wiesoftware.com/greenchili/apisecure/login/forgetPassword", body,requestOptions).subscribe(res => {
+          this.http.post("http://greenchili.ca/apisecure/login/forgetPassword", body,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -209,7 +209,7 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.get(" http://wiesoftware.com/greenchili/apisecure/location/locations/",requestOptions).subscribe(res => {
+          this.http.get(" http://greenchili.ca/apisecure/location/locations/",requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -227,7 +227,7 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           
-          this.http.get("http://wiesoftware.com/greenchili/apisecure/location/locationMenu/"+ id,requestOptions).subscribe(res => {
+          this.http.get("http://greenchili.ca/apisecure/location/locationMenu/"+ id,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -247,7 +247,7 @@ export class AuthService {
             'Accept': 'application/json',            
           });
           const requestOptions = new RequestOptions({ headers: headers });
-          this.http.get(" http://wiesoftware.com/greenchili/apisecure/location/locationMenuDishes/"+location_id+"/"+id ,requestOptions).subscribe(res => {
+          this.http.get(" http://greenchili.ca/apisecure/location/locationMenuDishes/"+location_id+"/"+id ,requestOptions).subscribe(res => {
           resolve(res.json());
            },(err) => {
             reject(err);
@@ -263,7 +263,7 @@ export class AuthService {
             });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.get("http://wiesoftware.com/greenchili/apisecure/location/locationDishDetails/"+id,requestOptions).subscribe(res => {
+          this.http.get("http://greenchili.ca/apisecure/location/locationDishDetails/"+id,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
@@ -283,12 +283,239 @@ export class AuthService {
           });
           const requestOptions = new RequestOptions({ headers: headers });
           //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
-          this.http.post("  http://wiesoftware.com/greenchili/apisecure/payment/paymentBilling", body,requestOptions).subscribe(res => {
+          this.http.post("  http://greenchili.ca/apisecure/payment/paymentBilling", body,requestOptions).subscribe(res => {
            resolve(res.json());
            },(err) => {
             reject(err);
           });
     })
   }
+
+
+
+
+
+
+
+
+
+
+    //Perfect running testing api functions
+  // UserLogin(body){ 
+  //   return new Promise((resolve,reject) => {
+  //     //let body = 'email=' + this.validations_form.value.email + '&password=' + this.validations_form.value.password;
+  //     //email=ram123@gmal.com&password=Admi@123
+  //     var headers = new Headers({
+  //          //'Content-Type': 'application/json; charset=utf-8',
+  //         // 'Accept': 'application/json', 
+  //         'content-Type': 'application/x-www-form-urlencoded',
+  //       'Accept': 'application/json'           
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         this.http.post("http://wiesoftware.com/greenchili/apisecure/login/loginUsers/", body,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          this.response= res.json(); 
+  //          console.log(this.response);
+  //          },(err) => {
+  //           reject(err);
+  //           console.log(err);
+  //         });
+  //   });
+  // }
+  
+  // SignUp(body) {
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',            
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.post("http://wiesoftware.com/greenchili/apisecure/login/registerUsers", body,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);            
+  //         });
+  //   });
+  // }
+
+  // myOrders(user_id){
+  //   return new Promise((resolve,reject) => {
+      
+      
+  //     var headers = new Headers({
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',                      
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.get(" http://wiesoftware.com/greenchili/apisecure/order/orderHistory/"+user_id,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // UserDetails(user_id){
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',
+  //           });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.get("http://wiesoftware.com/greenchili/apisecure/userDetails/"+user_id,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //           });
+  //   });
+  // }
+  
+  // UpdateProfile(user_id,body){
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',
+  //           });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.post("http://wiesoftware.com/greenchili/apisecure/userProfileEdit/"+user_id,body,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+            
+  //         });
+  //   });
+  // }
+
+  // OnlineReservation(body){
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({
+  //       'content-Type': 'application/x-www-form-urlencoded',
+  //       'Accept': 'application/json'
+  //     });
+  //     const requestOptions = new RequestOptions({headers: headers});
+  //     this.http.post("http://wiesoftware.com/greenchili/apisecure/onlineReservation",body, requestOptions).subscribe(res =>{
+  //       resolve(res.json());
+  //     }, (err) => {
+  //       reject(err);
+  //     });
+  //   });
+  // }
+
+  // ResetPwd(body){
+  //   return new Promise((resolve,reject) => {      
+  //     var headers = new Headers({
+            
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',                      
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.post("http://wiesoftware.com/greenchili/apisecure/login/forgetPassword", body,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // LocationData(){
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({
+  //           //'X-API-KEY': '123run',
+  //           //"Authorization": 'Basic',
+  //           //'username': 'devpankaj',
+  //           //'password': 'devpankaj',
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',
+  //           //'Access-Control-Allow-Methods': 'POST',
+            
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.get(" http://wiesoftware.com/greenchili/apisecure/location/locations/",requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // LocationList(id){
+  //   return new Promise((resolve,reject) => {
+      
+  //     var headers = new Headers({
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json', 
+                       
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+          
+  //         this.http.get("http://wiesoftware.com/greenchili/apisecure/location/locationMenu/"+ id,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // DishList(location_id,id){
+  //   return new Promise((resolve,reject) => {
+      
+  //     var headers = new Headers({
+  //           //'X-API-KEY': '123run',
+  //           //"Authorization": 'Basic',
+  //           //'username': 'devpankaj',
+  //           //'password': 'devpankaj',
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',            
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         this.http.get(" http://wiesoftware.com/greenchili/apisecure/location/locationMenuDishes/"+location_id+"/"+id ,requestOptions).subscribe(res => {
+  //         resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // ViewDish(id){
+  //   return new Promise((resolve,reject) => {
+  //     var headers = new Headers({          
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',       
+  //           });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.get("http://wiesoftware.com/greenchili/apisecure/location/locationDishDetails/"+id,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
+
+  // PlaceOrder(body){
+  //   return new Promise((resolve,reject) => {              
+  //     var headers = new Headers({
+  //           //"Authorization": 'Basic',
+  //           //"username": 'devpankaj',
+  //           //"password": 'devpankaj',
+  //           //"X-API-KEY": '123run',
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           'Accept': 'application/json',            
+  //         });
+  //         const requestOptions = new RequestOptions({ headers: headers });
+  //         //let body = [{"email": this.validations_form.value.email, "password": this.validations_form.value.password}];
+  //         this.http.post("  http://wiesoftware.com/greenchili/apisecure/payment/paymentBilling", body,requestOptions).subscribe(res => {
+  //          resolve(res.json());
+  //          },(err) => {
+  //           reject(err);
+  //         });
+  //   })
+  // }
 
 }
